6e0e442fa28eed668274f520c10d9ff5bf6e574b
Gama <maga499599@gmail.com>
2024-12-14 15:45:49 +0600

    Create the PaymentAcceptor add into Get current information Add money Reset amount implementation for a coin acceptor.

--- src/model/CoinAcceptor.java
+++ src/model/CoinAcceptor.java
@@ -1,14 +1,24 @@
 package model;
 
-public class CoinAcceptor {
+public class CoinAcceptor implements PaymentAcceptor{
     private int amount;
 
     public CoinAcceptor(int amount) {
         this.amount = amount;
     }
 
+    @Override
+    public void addAmount(int amount) {
+
+    }
+
     public int getAmount() {
         return amount;
+    }
+
+    @Override
+    public void resetAmount() {
+
     }
 
     public void setAmount(int amount) {
